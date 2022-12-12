9da1e4051ceba193003b57c4352ddc81065eb89b
NicklasSoderlund <nicklassoderlund96@gmail.com>
2022-12-12 18:02:55 +0100

    started function

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,21 +1,21 @@
 import { Product } from "./models/product";
 
 let products = [
-    new Product(new URL("https://imgur.com/a/wLShfuG"), 
+    new Product("https://i.imgur.com/yc4KP66.png", 
     "SAMSUNG Q70B QLED Smart TV",
     "4K",
     "120Hz",
     "65''",
     "5000kr",
     "TV"),
-    new Product(new URL("https://imgur.com/a/c6mBJcK"),
+    new Product("https://i.imgur.com/4K1Vgp8.png",
     "SHARP 55BL2EA UHD Android TV",
     "4K",
     "50Hz",
     "55''",
     "3000kr",
     "TV"),
-    new Product(new URL("https://imgur.com/a/Rg7ld86"),
+    new Product("https://i.imgur.com/qhScFyv.png",
     "SHARP 43EL4EA Android TV",
     "4K",
     "50Hz",
@@ -23,21 +23,21 @@ let products = [
     "4000kr",
     "TV"),
 
-    new Product(new URL("https://imgur.com/a/M8JTHlj"),
+    new Product("https://i.imgur.com/NFj2lxF.png",
     "MSI Optix G273QF - Gamingskärm",
     "WQHD",
     "165Hz",
     "27''",
     "4000kr",
     "Monitor"),
-    new Product(new URL("https://imgur.com/a/GnGLFyR"),
+    new Product("https://i.imgur.com/trakBW3.png",
     "AOC 25G3ZM/BK",
     "Full HD",
     "240Hz",
     "24.5''",
     "4500kr",
     "Monitor"),
-    new Product(new URL("https://imgur.com/a/GnGLFyR"),
+    new Product("https://i.imgur.com/QFcEtH5.png",
     "ACER Nitro XV272L",
     "Full HD",
     "144Hz",
@@ -45,5 +45,26 @@ let products = [
     "2500kr",
     "Monitor"),
 ]
+
+function createProductsHTML() {
+    let monitorsContainer = document.getElementById("product-page__monitors") as HTMLElement;
+    let tvScreensContainer = document.getElementById("product-page__tv-screens") as HTMLElement;
+ 
+
+    for (let i = 0; i < products.length; i++) {
+        let ProductLi = document.createElement("li");
+        let ProductContainer = document.createElement("div");
+        let ProductImage = document.createElement("img");
+        
+        ProductImage.setAttribute("src", products[i].image);
+        ProductImage.setAttribute("alt", "Picture of product");
+
+        ProductContainer.appendChild(ProductImage);
+        ProductLi.appendChild(ProductContainer);
+        monitorsContainer.appendChild(ProductLi);
+        
+    }
+}
+createProductsHTML()
 
 console.log(products);
