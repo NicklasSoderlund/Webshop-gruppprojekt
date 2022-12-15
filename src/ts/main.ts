5f390d0a848f2cd3f91b5ddd41ce2ecf9d0e9140
NicklasSoderlund <nicklassoderlund96@gmail.com>
2022-12-15 19:42:52 +0100

    productfunction progress

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -5,46 +5,52 @@ let products = [
     "SAMSUNG Q70B QLED Smart TV",
     "4K",
     "120Hz",
-    "65''",
-    "5000kr",
+    "65\"",
+    5000,
     "TV"),
     new Product("https://i.imgur.com/4K1Vgp8.png",
     "SHARP 55BL2EA UHD Android TV",
     "4K",
     "50Hz",
-    "55''",
-    "3000kr",
+    "55\"",
+    3000,
     "TV"),
     new Product("https://i.imgur.com/qhScFyv.png",
     "SHARP 43EL4EA Android TV",
     "4K",
     "50Hz",
-    "43''",
-    "4000kr",
+    "43\"",
+    4000,
     "TV"),
 
     new Product("https://i.imgur.com/NFj2lxF.png",
     "MSI Optix G273QF - Gamingskärm",
     "WQHD",
     "165Hz",
-    "27''",
-    "4000kr",
+    "27\"",
+    4000,
     "Monitor"),
     new Product("https://i.imgur.com/trakBW3.png",
     "AOC 25G3ZM/BK",
     "Full HD",
     "240Hz",
-    "24.5''",
-    "4500kr",
+    "24.5\"",
+    4500,
     "Monitor"),
     new Product("https://i.imgur.com/QFcEtH5.png",
     "ACER Nitro XV272L",
     "Full HD",
     "144Hz",
-    "27''",
-    "2500kr",
+    "27\"",
+    2500,
     "Monitor"),
 ]
+
+function checkStorage() {
+    if (localStorage.getItem("shoppingCart") === null) {
+        localStorage.setItem("shoppingCart", "")
+    }
+}
 
 function createProductsHTML() {
     let monitorsContainer = document.getElementById("product-page__monitors") as HTMLElement;
@@ -52,16 +58,47 @@ function createProductsHTML() {
  
 
     for (let i = 0; i < products.length; i++) {
-        let ProductLi = document.createElement("li");
-        let ProductContainer = document.createElement("div");
-        let ProductImage = document.createElement("img");
-        
-        ProductImage.setAttribute("src", products[i].image);
-        ProductImage.setAttribute("alt", "Picture of product");
+        let productLi = document.createElement("li");
+        let productContainer = document.createElement("div");
+        let productImage = document.createElement("img");
+        let productName = document.createElement("h5");
+        let productSpecsList = document.createElement("ul");
+        let firstSpec = document.createElement("li");
+        let secondSpec = document.createElement("li");
+        let thirdSpec = document.createElement("li");
 
-        ProductContainer.appendChild(ProductImage);
-        ProductLi.appendChild(ProductContainer);
-        monitorsContainer.appendChild(ProductLi);
+        productImage.src = products[i].image;
+        productImage.setAttribute("alt", "Picture of product");
+        productName.innerHTML = products[i].name;
+        firstSpec.innerHTML = products[i].quality;
+        secondSpec.innerHTML = products[i].inches;
+        thirdSpec.innerHTML = products[i].hz;
+
+        let productBottomContainer = document.createElement("div");
+        let productPrice = document.createElement("h6");
+        let addToCart = document.createElement("button");
+        productPrice.innerHTML = JSON.stringify(products[i].price);
+        addToCart.innerHTML = "Lägg till i <i class=\"fa-solid fa-basket-shopping\">"
+        addToCart.addEventListener("click", () => {
+            localStorage.setItem("")
+        } )
+
+        productBottomContainer.appendChild(productPrice);
+        productBottomContainer.appendChild(addToCart);
+
+
+        productSpecsList.appendChild(firstSpec);
+        productSpecsList.appendChild(secondSpec);
+        productSpecsList.appendChild(thirdSpec);
+
+        
+        productContainer.appendChild(productImage);
+        productContainer.appendChild(productName);
+        productContainer.appendChild(productSpecsList);
+        productContainer.appendChild(productBottomContainer);
+
+        productLi.appendChild(productContainer);
+        monitorsContainer.appendChild(productLi);
         
     }
 }
